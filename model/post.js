d9fd6fafe75c510d487e0dff592e55ca8ce62bfe
Vic <syuan0212@gmail.com>
2022-04-20 08:56:30 +0800

    fix model post

--- model/post.js
+++ model/post.js
@@ -3,30 +3,30 @@ const postScheam = new Schema(
   {
     name: {
       type: String,
-      required: [true, '貼文姓名未填寫'],
+      required: [true, '貼文欄位 {PATH} 未填寫'],
     },
     tags: [
       {
         type: String,
-        required: [true, '貼文標籤 tags 未填寫']
+        required: [true, '貼文欄位 {PATH} 未填寫']
       }
     ],
     type: {
       type: String,
-      required: [true, '貼文類型 type 未填寫']
+      required: [true, '貼文欄位 {PATH} 未填寫']
     },
     image: {
       type: String,
       default: '',
     },
-    cratedAt: {
+    createdAt: {
       type: Date,
       default: Date.now,
       select: false
     },
     content: {
       type: String,
-      required: [true, '內容未填寫'],
+      required: [true, '貼文欄位 {PATH} 未填寫'],
     },
     likes: {
       type: Number,
